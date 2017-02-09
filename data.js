78d6a430fbe735f4f56bf27ee5c4c02124cf053d
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-02-09 16:15:48 +0000

    Walkthrough for Tannyoky Guns Driveway

---
+++ data.js
@@ -0,0 +1,375 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010459",
+      "name": "R0010459",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5169246207698048,
+        "pitch": 0.0835157273102336,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7815184656916347,
+          "pitch": 0.12529958282636855,
+          "rotation": 0,
+          "target": "1-r0010458"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0010458",
+      "name": "R0010458",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5451919089480386,
+        "pitch": 0.05111943203179159,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.370920127665542,
+          "pitch": 0.150770393890868,
+          "rotation": 0,
+          "target": "0-r0010459"
+        },
+        {
+          "yaw": 1.751315174489834,
+          "pitch": 0.09966950755069348,
+          "rotation": 0,
+          "target": "2-r0010455"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010455",
+      "name": "R0010455",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.203715488802482,
+        "pitch": 0.08808203701653561,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9769465532524881,
+          "pitch": 0.07941197266536726,
+          "rotation": 0,
+          "target": "1-r0010458"
+        },
+        {
+          "yaw": -1.297659998146795,
+          "pitch": 0.13551613513291016,
+          "rotation": 0,
+          "target": "3-r0010454"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-r0010454",
+      "name": "R0010454",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.22108348909470266,
+        "pitch": 0.3040927468427945,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.121983605702969,
+          "pitch": 0.08174876323236546,
+          "rotation": 0,
+          "target": "2-r0010455"
+        },
+        {
+          "yaw": -0.17806351977653634,
+          "pitch": 0.17645611848997156,
+          "rotation": 0,
+          "target": "4-r0010453"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-r0010453",
+      "name": "R0010453",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.24623389637934068,
+        "pitch": 0.184552839463219,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.686831527338061,
+          "pitch": 0.0934172266807689,
+          "rotation": 0,
+          "target": "3-r0010454"
+        },
+        {
+          "yaw": 0.6150068532582189,
+          "pitch": 0.037583140958606265,
+          "rotation": 0,
+          "target": "5-r0010452"
+        },
+        {
+          "yaw": -0.5418620853021459,
+          "pitch": 0.18428331324969704,
+          "rotation": 0,
+          "target": "6-r0010451"
+        },
+        {
+          "yaw": 0.0529894719142483,
+          "pitch": 0.06712216206746291,
+          "rotation": 0,
+          "target": "7-r0010450"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-r0010452",
+      "name": "R0010452",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.5967084736158315,
+        "pitch": 0.004192856359734876,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6918997955593573,
+          "pitch": 0.216409849736527,
+          "rotation": 0,
+          "target": "4-r0010453"
+        },
+        {
+          "yaw": 2.6207383441739056,
+          "pitch": 0.030879868206804773,
+          "rotation": 0,
+          "target": "6-r0010451"
+        },
+        {
+          "yaw": -2.9911050465723683,
+          "pitch": 0.14019646366367589,
+          "rotation": 0,
+          "target": "7-r0010450"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-r0010451",
+      "name": "R0010451",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.4365158685828892,
+        "pitch": 0.08725975931601226,
+        "fov": 1.9665011695721961
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.675716401963861,
+          "pitch": 0.0895285845495053,
+          "rotation": 0,
+          "target": "5-r0010452"
+        },
+        {
+          "yaw": -2.030276827522208,
+          "pitch": 0.22128704679349198,
+          "rotation": 0,
+          "target": "4-r0010453"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-r0010450",
+      "name": "R0010450",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.19090964835725,
+        "pitch": -0.06009586527805233,
+        "fov": 1.9670779013366633
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2480308551336847,
+          "pitch": 0.2048784551430014,
+          "rotation": 0,
+          "target": "5-r0010452"
+        },
+        {
+          "yaw": -1.3228969782052236,
+          "pitch": 0.2431241092464873,
+          "rotation": 0,
+          "target": "4-r0010453"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Tannyoky Driveway 09.02.17",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
